77ee0716b3d717d922f02995b03f7ec2e093442d
Cowell & Co Web Design <31576941+CowellandCoWebDesign@users.noreply.github.com>
2020-10-22 13:58:40 -0700

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
 	})(jQuery);
 
 	// input text for typing animation
-	$("#holder").writeText("Business Strategy and Marketing Consultant");
+	$("#holder").writeText("Business Strategy and Technical Consultant");
 
 	/* ####### HERO SECTION ####### */
 
